c8162cbfbbc239ea2afd66a44a385bd873379bbf
Artemio <artemiolucero@gmail.com>
2022-09-03 22:50:44 -0300

    Added views for index and posts

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from django.core.paginator import Paginator
 from .models import Post, Categoria
+from datetime import date
 
 def home(request):
     queryset = request.GET.get('buscar')
     posts = Post.objects.filter(estado=True)
+    fecha = date.today()
+    anio = fecha.strftime('%Y')
+    print(anio)
 
     if queryset:
         posts = Post.objects.filter(
@@ -16,7 +20,9 @@ def home(request):
     page = request.GET.get('page')
     posts = paginator.get_page(page)
 
-    return render(request, 'index.html', {'posts': posts})
+    context = {'posts': posts, 'anio': anio} 
+
+    return render(request, 'index.html', context)
 
 def detalle_post(request, slug):
     post = get_object_or_404(Post, slug=slug)
